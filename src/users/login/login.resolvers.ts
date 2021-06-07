39e5e7aed2f14c17cdcb8e79a35e7d81a203e0d8
yezii <1126yezy@gmail.com>
2021-06-07 22:30:41 +0900

    Fix login resolvers

--- src/users/login/login.resolvers.ts
+++ src/users/login/login.resolvers.ts
@@ -5,33 +5,46 @@ import { Resolvers } from "../../types";
 const resolvers: Resolvers = {
   Mutation: {
     login: async (_, { username, password }, { client }) => {
-      // Find user with args.username
-      const user = client.user.findFirst({ where: { username } });
-      if (!user) {
+      try {
+        // Find user with args.username
+        const user = await client.user.findFirst({ where: { username } });
+        if (!user) {
+          return {
+            ok: false,
+            error: "User is not found.",
+          };
+        }
+
+        // Check password with args.password
+        const passwordOk = await bcrypt.compare(
+          password,
+          (
+            await user
+          ).password
+        );
+        if (!passwordOk) {
+          return {
+            ok: false,
+            error: "Incorrect Password",
+          };
+        }
+
+        // Issue a token and send it to the user
+        const token = await jwt.sign(
+          { id: (await user).id },
+          process.env.SECRET_KEY
+        );
+
         return {
-          ok: false,
-          error: "User is not found.",
+          ok: true,
+          token,
+        };
+      } catch (error) {
+        return {
+          ok: true,
+          error: error.message,
         };
       }
-
-      // Check password with args.password
-      const passwordOk = await bcrypt.compare(password, (await user).password);
-      if (!passwordOk) {
-        return {
-          ok: false,
-          error: "Incorrect Password",
-        };
-      }
-
-      // Issue a token and send it to the user
-      const token = await jwt.sign(
-        { id: (await user).id },
-        process.env.SECRET_KEY
-      );
-      return {
-        ok: true,
-        token,
-      };
     },
   },
 };
