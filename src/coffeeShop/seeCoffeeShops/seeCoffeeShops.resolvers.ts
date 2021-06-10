64d76fa6af496cc894edc4c6fa9e1314b4f2b929
yezii <1126yezy@gmail.com>
2021-06-10 20:22:33 +0900

    Fix seeCoffeeShops

--- src/coffeeShop/seeCoffeeShops/seeCoffeeShops.resolvers.ts
+++ src/coffeeShop/seeCoffeeShops/seeCoffeeShops.resolvers.ts
@@ -2,11 +2,30 @@ import { Resolvers } from "../../types";
 
 const resolvers: Resolvers = {
   Query: {
-    seeCoffeeShops: (_, { offset }, { client }) =>
+    seeCoffeeShops: (_, { offset }, { client, loggedInUser }) =>
       client.coffeeShop.findMany({
         take: 5,
         skip: offset,
         include: { photos: true, user: true, categories: true },
+        where: {
+          OR: [
+            {
+              user: {
+                followers: {
+                  some: {
+                    id: loggedInUser.id,
+                  },
+                },
+              },
+            },
+            {
+              userId: loggedInUser.id,
+            },
+          ],
+        },
+        orderBy: {
+          id: "desc",
+        },
       }),
   },
 };
